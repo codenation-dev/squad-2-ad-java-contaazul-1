fd3785d8bb6136e23225288bbb4214ecdaca052f
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-04-12 20:09:36 -0300

    Primeiro Commit - subindo projeto/estrutura

---
+++ src/test/java/com/javawomen/CentraldeErros/CentralDeErrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javawomen.CentraldeErros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralDeErrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
