121f56588f1dfda96665dad76bda2a5608206220
Alice Borges <aliceborges@outlook.com.br>
2020-04-20 17:50:58 -0300

    Entidade de usuários criada

--- src/main/java/com/javawomen/CentraldeErros/model/UserModel.java
+++ src/main/java/com/javawomen/CentraldeErros/model/UserModel.java
@@ -1,12 +1,50 @@
-/**
- * 
- */
 package com.javawomen.CentraldeErros.model;
 
-/**
- * @author MarleiMaria
- *
- */
-public class UserModel {
+import java.security.Timestamp;
 
+public class UserModel {
+	private Long id;
+	private Timestamp createdAt;
+	private String name;
+	private String email;
+	private String password;
+	
+	public UserModel (String name, String email, String password) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
