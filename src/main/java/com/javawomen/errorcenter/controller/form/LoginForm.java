daeab15a4858dc535e78c9b008f9e6df85c0f9f3
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-05-01 08:35:18 -0300

    adicionado loginform

---
+++ src/main/java/com/javawomen/errorcenter/controller/form/LoginForm.java
@@ -0,0 +1,32 @@
+package com.javawomen.errorcenter.controller.form;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+public class LoginForm {
+
+	private String email;
+	
+	private String passCode;
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassCode(String passCode) {
+		this.passCode = passCode;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassCode() {
+		return passCode;
+	}
+
+	public UsernamePasswordAuthenticationToken convert() {//metodo para o autentication manager
+		return new UsernamePasswordAuthenticationToken(this.email, this.passCode);
+	}
+	
+	
+}
