fc299a67a5383aa1c85fc3cc3f7d0391940d4977
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-05-01 08:07:22 -0300

    Adicionado UserRepository

--- src/main/java/com/javawomen/errorcenter/repository/UserRepository.java
+++ src/main/java/com/javawomen/errorcenter/repository/UserRepository.java
@@ -1,12 +1,25 @@
-/**
- * 
- */
 package com.javawomen.errorcenter.repository;
 
-/**
- * @author MarleiMaria
- *
- */
-public class UserRepository {
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.javawomen.errorcenter.model.User;
+
+
+//extends o crudRepository
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{	
+	//save ok testado
+	//delete
+	//findAll ok
+	//findByEmail ok
+	//put nao outr
+	
+	
+	Optional<User> findByEmail(@Param("email") String email);
+	
 }
