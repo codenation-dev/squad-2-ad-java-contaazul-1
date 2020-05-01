50fa36e99c8013587bf716976d21573b7a61b1c5
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-05-01 07:57:43 -0300

    Adicionada classe Entidade Role

---
+++ src/main/java/com/javawomen/errorcenter/model/Role.java
@@ -0,0 +1,52 @@
+package com.javawomen.errorcenter.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "role") //role == papel, perfil
+public class Role implements GrantedAuthority { 
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@Column(name="perfil_id")
+	private Long id;
+	
+	private String roleName; //moderador, admin...
+ 
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	@Override
+	public String getAuthority() {
+		return this.roleName;
+	}
+	
+	
+	
+	
+}
