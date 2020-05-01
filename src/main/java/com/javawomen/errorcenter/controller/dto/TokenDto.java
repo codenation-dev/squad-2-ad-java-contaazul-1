2459d7676fd25aed5476f0034cfa54c2dbe6fdd8
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-05-01 08:37:16 -0300

    adicionado tokendto

---
+++ src/main/java/com/javawomen/errorcenter/controller/dto/TokenDto.java
@@ -0,0 +1,23 @@
+package com.javawomen.errorcenter.controller.dto;
+
+public class TokenDto {
+	
+	private String token;
+	private String type; // =="Bearer"
+	
+	public TokenDto(String token, String type) {
+		this.token = token;
+		this.type = type;	
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public String getType() {
+		return type;
+	}
+	
+	
+
+}
