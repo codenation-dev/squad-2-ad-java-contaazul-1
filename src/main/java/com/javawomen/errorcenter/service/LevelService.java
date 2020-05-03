da5d979d95e322aae5ec7fb1bae6ad0e45b692a4
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-05-03 19:34:36 -0300

    Adicionado classe LevelService

--- src/main/java/com/javawomen/errorcenter/service/LevelService.java
+++ src/main/java/com/javawomen/errorcenter/service/LevelService.java
@@ -1,5 +1,39 @@
 package com.javawomen.errorcenter.service;
 
-public class LevelService {
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.javawomen.errorcenter.model.Level;
+import com.javawomen.errorcenter.repository.LevelRepository;
+
+@Service
+public class LevelService implements ServiceInterface<Level>{
+
+	@Autowired
+	private LevelRepository levelRepository;
+	
+	@Override
+	public Page<Level> findAll(Pageable paginacao) {
+		return levelRepository.findAll(paginacao);
+	}
+
+	@Override
+	public Optional<Level> findById(Long id) {
+		return levelRepository.findById(id);
+	}
+
+	@Override
+	public Level save(Level object) {
+		return levelRepository.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		levelRepository.deleteById(id);		
+	}
 
 }
