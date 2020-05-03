97c4def1bc419a5df54ffd3487e583a546558751
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-05-03 19:34:58 -0300

    Adicionado classe EnvironmentService

--- src/main/java/com/javawomen/errorcenter/service/EnvironmentService.java
+++ src/main/java/com/javawomen/errorcenter/service/EnvironmentService.java
@@ -1,5 +1,42 @@
 package com.javawomen.errorcenter.service;
 
-public class EnvironmentService {
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.javawomen.errorcenter.model.Environment;
+import com.javawomen.errorcenter.repository.EnvironmentRepository;
+import com.javawomen.errorcenter.service.ServiceInterface;
+
+@Service
+public class EnvironmentService implements ServiceInterface<Environment>{
+
+	@Autowired
+	private EnvironmentRepository environmentRepository;
+
+	@Override
+	public Page<Environment> findAll(Pageable paginacao) {
+		return environmentRepository.findAll(paginacao);
+	}
+
+	@Override
+	public Optional<Environment> findById(Long id) {
+		return environmentRepository.findById(id);
+	}
+
+	@Override
+	public Environment save(Environment object) {
+		return environmentRepository.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		environmentRepository.deleteById(id);		
+	}
+	
+
 
 }
