cc10760ac710d3cb43897608f9cfe56428f4b472
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-05-02 23:31:26 -0300

    Alterado retorno do método para Optional<Environment>

--- src/main/java/com/javawomen/errorcenter/repository/EnvironmentRepository.java
+++ src/main/java/com/javawomen/errorcenter/repository/EnvironmentRepository.java
@@ -1,6 +1,8 @@
 package com.javawomen.errorcenter.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,6 @@ import com.javawomen.errorcenter.model.Environment;
 @Repository
 public interface EnvironmentRepository extends JpaRepository<Environment, Long>{
 
-	Environment findByName(String name);
-
+	//Environment findByName(String name);
+	Optional<Environment> findByName(String name);
 }
