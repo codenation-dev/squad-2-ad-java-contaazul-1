e0bb37fb61e146b85c04fca279d1981985ed3700
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-05-11 13:01:28 -0300

    Injetado o repository feito acesso ao controller

--- src/main/java/com/javawomen/errorcenter/service/LogService.java
+++ src/main/java/com/javawomen/errorcenter/service/LogService.java
@@ -1,7 +1,9 @@
 package com.javawomen.errorcenter.service;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -15,40 +17,59 @@ import com.javawomen.errorcenter.repository.LogRepository;
  */
 
 @Service
-public class LogService implements ServiceInterface<Log>{
-
+public class LogService{// implements ServiceInterface<Log> {
+	@Autowired
 	LogRepository logRepository;
 
-	@Override
+	//@Override
 	public Page<Log> findAll(Pageable paginacao) {
 		return logRepository.findAll(paginacao);
 	}
 
-	@Override
-	public Optional<Log> findById(Long id) {
+	//@Override
+	public Optional<Log> findByIds(Long id) {
 		return logRepository.findById(id);
 	}
 
-	@Override
+	//@Override
 	public Log save(Log object) {
 		return logRepository.save(object);
 	}
 
-	@Override
+	//@Override
 	public void deleteById(Long id) {
 		logRepository.deleteById(id);
 	}
-	
-	//falta implementar:
-	
-	//Page<Log> logRepository.findByEnvironmentName(String nameEnvironment, Pageable paginacao);
 
-	//Page<LogDto> LogDto.converter(Page<Log> logs);
+	// OK
+	public List<Log> findByLevel(String nameLevel) {
+		return logRepository.findByLevelName(nameLevel);
+	}
+
+	// OK
+	public List<Log> findByEnvironment(String nameEnvironment) {
+		return logRepository.findByEnvironmentName(nameEnvironment); //Page<Log> // Pageable paginacao);
+	}
 	
-	//List<Log> logRepository.findByLevelName(String level);
+	// OK
+	public List<Log> findByDescription(String description) {
+		return logRepository.findByDescription(description);
+	}
+
+	//OK
+	public List<Log> findByOrigin(String origin) {				
+		return logRepository.findByOrigin(origin);
+	}
+
 	
-	//List<LogDto> LogDto.converterToLog(List<Log> logs);
-	
-	//Log form.converter(LevelRepository levelRepository, EnvironmentRepository environmentRepository);
+	// falta implementar: -------------------------------------------
+
+
+	// Page<LogDto> LogDto.converter(Page<Log> logs);
+
+	// List<LogDto> LogDto.converterToLog(List<Log> logs);
+
+	// Log form.converter(LevelRepository levelRepository, EnvironmentRepository
+	// environmentRepository);
 
 }
