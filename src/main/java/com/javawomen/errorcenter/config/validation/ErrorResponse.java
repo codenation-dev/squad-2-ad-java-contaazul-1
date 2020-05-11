aed1ce32fdd273e7ae6885c63c2890e396221685
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-05-11 13:02:36 -0300

    Desenvolvido classe de resposta de erro para o handler advice

---
+++ src/main/java/com/javawomen/errorcenter/config/validation/ErrorResponse.java
@@ -0,0 +1,38 @@
+package com.javawomen.errorcenter.config.validation;
+
+import java.util.List;
+
+public class ErrorResponse {
+	
+    private final String message;
+    private final int code;
+    private final String status;
+    private final String objectName;
+    private final List<ObjectError> errors;
+    
+    //nao pode usar o metodo setter
+	public ErrorResponse(String message, int code, String status, String objectName, List<ObjectError> errors) {
+		this.message = message;
+		this.code = code;
+		this.status = status;
+		this.objectName = objectName;
+		this.errors = errors;
+	}
+	
+
+	public String getMessage() {
+		return message;
+	}
+	public int getCode() {
+		return code;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public String getObjectName() {
+		return objectName;
+	}
+	public List<ObjectError> getErrors() {
+		return errors;
+	}
+}
