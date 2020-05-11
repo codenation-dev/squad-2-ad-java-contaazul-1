cc3acd19ebbb00a9dd9c8b0a0cf64e39f85f0639
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-05-11 12:53:33 -0300

    Adicionado classe ObjectError para montar a mensagem de erro do Handler

---
+++ src/main/java/com/javawomen/errorcenter/config/validation/ObjectError.java
@@ -0,0 +1,29 @@
+package com.javawomen.errorcenter.config.validation;
+
+public class ObjectError {//ErroDeFormularioDto 
+	
+    private final String message;
+    private final String field;
+    private final Object parameter;
+    
+	public ObjectError(String message, String field, Object parameter) {
+		this.message = message;
+		this.field = field;
+		this.parameter = parameter;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public Object getParameter() {
+		return parameter;
+	}
+	
+	
+    
+}
