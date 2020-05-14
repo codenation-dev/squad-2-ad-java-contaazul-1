87e47b343233403248ff1e12dd065378d716956d
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-05-14 19:15:54 -0300

    Transferido método para a classe UserService

--- src/main/java/com/javawomen/errorcenter/controller/form/UserForm.java
+++ src/main/java/com/javawomen/errorcenter/controller/form/UserForm.java
@@ -1,63 +1,77 @@
 package com.javawomen.errorcenter.controller.form;
 
+import java.util.Optional;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.javawomen.errorcenter.config.validation.ResourceNotFoundException;
+import com.javawomen.errorcenter.model.Role;
 import com.javawomen.errorcenter.model.User;
-
+import com.javawomen.errorcenter.service.RoleService;
 
 public class UserForm {
+
+	@NotNull(message = "{name.not.null}")
+	@NotEmpty(message = "{name.not.empty}")
+	@NotBlank(message = "{name.not.blank}")
+	@Length(min = 3, max = 50)
+	private String name;
+
+	@NotBlank(message = "{email.not.blank}")
+	@NotNull(message = "{email.not.null}")
+	@NotEmpty(message = "{email.not.empty}")
+	@Email(message = "{email.not.valid}")	
+	private String email;
+
+	@NotNull(message = "{password.not.null}")
+	@NotEmpty(message = "{password.not.empty}")
+	@NotBlank(message = "{password.not.blank}")
+	@Length(min = 8)
+	private String password;
 	
-	@NotNull(message="{name.not.null}")
-	@NotEmpty(message="{name.not.empty}")
-	@NotBlank(message="{name.not.blank}")
-	@Length(min=3,max=50)
-    private String name;
+	//private String roleName = "ROLE_USER";
+
 	
-	@NotNull(message="{email.not.null}")
-	@NotEmpty(message="{email.not.empty}")
-	@Email(message="{email.not.valid}")
-	@NotBlank(message="{email.not.blank}")
-	@Length(min=9,max=100)
-    private String email;
-	
-	@NotNull(message="{name.not.null}")
-	@NotEmpty(message="{name.not.empty}")
-	@NotBlank(message="{name.not.blank}")
-	@Length(min = 6,max=100)
-    private String password;
-    
 	
 	public String getName() {
 		return name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
 	
-	public User converter() {
-		return new User(name, email, password);
-	}
+	//public String getRoleName() {
+		//return roleName;
+	//}
+
+	//public User converter(RoleService roleService) {
+		//Optional<Role> roleOptional = roleService.findByName(roleName);
+		//if(!roleOptional.isPresent())throw new ResourceNotFoundException("Role não encontrado.");
+		//return new User(this.name, new BCryptPasswordEncoder().encode(this.password), this.email, roleOptional.get());
+	//}
 	
-	// verificar isso 
-	//public User converter() {
-    //return new User(this.name, new BCryptPasswordEncoder().encode(this.password), this.email);
-  	//}
+	
 }
