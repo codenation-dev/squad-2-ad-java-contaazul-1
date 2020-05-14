8f90015c72ed58169edca551a66de73d50689568
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-05-14 19:17:05 -0300

    Adicionado métodos para alteracao de role

--- src/main/java/com/javawomen/errorcenter/controller/UserController.java
+++ src/main/java/com/javawomen/errorcenter/controller/UserController.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
@@ -15,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 //import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -23,20 +25,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.javawomen.errorcenter.repository.UserRepository;
+import com.javawomen.errorcenter.service.RoleService;
+import com.javawomen.errorcenter.service.UserService;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import springfox.documentation.annotations.ApiIgnore;
+
 import com.javawomen.errorcenter.controller.form.UpdateUserForm;
 import com.javawomen.errorcenter.controller.form.UserForm;
+import com.javawomen.errorcenter.config.validation.ResourceNotFoundException;
 import com.javawomen.errorcenter.controller.dto.UserDto;
+import com.javawomen.errorcenter.model.Role;
 import com.javawomen.errorcenter.model.User;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+	// @Autowired
+	// private UserRepository userRepository;
 	@Autowired
-	private UserRepository userRepository;
+	private UserService userService;
+	@Autowired
+	private RoleService roleService;
 
-	// ------------------ GET ALL --------------------------------
+	// ------------------------- GET ALL --------------------------------
 
 	// http://localhost:8080/users ok: traz uma lista de users cadastrados
 	// @GetMapping
@@ -46,21 +60,26 @@ public class UserController {
 	// }
 
 	@GetMapping
+	@Cacheable("listOfUser") // importei o do spring, verififcar se nao tem q ser o javax
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "page", dataType = "integer", paramType = "query", value = "Pagina à ser carregada", defaultValue = "0"),
+			@ApiImplicitParam(name = "size", dataType = "integer", paramType = "query", value = "Quantidade de registros", defaultValue = "10"),
+			@ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query", value = "Ordenação dos registros") })
 	public Page<UserDto> getAllUsers(
-			@PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 10) Pageable paginacao) {
-		
-		Page<User> users = userRepository.findAll(paginacao);
+			@PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 10) @ApiIgnore Pageable paginacao) {
+		Page<User> users = userService.findAll(paginacao);
 		return UserDto.converter(users);
 	}
-	
-	
+
 	// ------------------ GET BY ID --------------------------------
 	// http://localhost:8080/users/{id} nao testado: traz o user cadastrado no id
-	@GetMapping("/{id}") //(value = "/{id}")
+	@GetMapping("/{id}") // (value = "/{id}")
 	public UserDto getUserById(@PathVariable Long id) {
-		Optional<User> user = userRepository.findById(id);
-		return UserDto.converterToUser(user);
-		
+		Optional<User> userOptional = userService.findById(id);
+		if (!userOptional.isPresent())
+			throw new ResourceNotFoundException("ID não encontrado.");
+		return UserDto.converterToUser(userOptional);
+
 	}
 
 	// ------------------ GET BY EMAIL--------------------------------
@@ -74,13 +93,11 @@ public class UserController {
 
 	// ------------------ POST --------------------------------
 	// o post é RequestBody: spring pega no corpo e nao na url eihn!
-	// testado ok: cadastra o user no bco
-	//trocar o nome do metodo
 	@PostMapping
 	@Transactional
 	public ResponseEntity<UserDto> registerUser(@RequestBody @Valid UserForm form, UriComponentsBuilder uriBuilder) {
-		User user = form.converter(); // form: dado do cliente
-		userRepository.save(user);
+		User user = userService.converter(roleService, form); // form: dado do cliente
+		userService.save(user);
 		URI uri = uriBuilder.path("/users/{id}").buildAndExpand(user.getId()).toUri();
 		return ResponseEntity.created(uri).body(new UserDto(user));
 	}
@@ -89,30 +106,45 @@ public class UserController {
 
 	@PutMapping("/{id}")
 	@Transactional
-	public ResponseEntity<UserDto> updateUser(@PathVariable Long id, @RequestBody @Valid UpdateUserForm form) {
-		Optional<User> optional = userRepository.findById(id);
-		if (optional.isPresent()) {
-			User user = form.update(id, userRepository);
-			return ResponseEntity.ok(new UserDto(user));
-		}
-		
-		return ResponseEntity.notFound().build();
+	public ResponseEntity<UserDto> updateUser(@PathVariable Long id, @RequestBody @Valid UserForm form) {
+		Optional<User> userOptional = userService.findById(id);
+		if (!userOptional.isPresent())
+			throw new ResourceNotFoundException("ID não encontrado.");
+		User user = userService.update(userOptional, form);
+		return ResponseEntity.ok(new UserDto(user));
+
+		// return ResponseEntity.notFound().build();
 	}
 
 	// ------------------ DELETE --------------------------------
 	@DeleteMapping("/{id}")
 	@Transactional
 	public ResponseEntity<UserDto> deleteUser(@PathVariable Long id) {
-		//ao colocar essa linha e o return comentado retorna apenas o Response headers sem o response body no swagger
-		//public ResponseEntity<?> deleteUser(@PathVariable Long id) {
-		Optional<User> optionalUser = userRepository.findById(id);
-		if (optionalUser.isPresent()) {
-			userRepository.deleteById(id);
-		//return ResponseEntity.ok().build();
-			return ResponseEntity.ok(UserDto.converterToUser(optionalUser));
-		}
+		// ao colocar essa linha e o return comentado retorna apenas o Response headers
+		// sem o response body no swagger
+		// public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+		Optional<User> userOptional = userService.findById(id);
+		if (!userOptional.isPresent())
+			throw new ResourceNotFoundException("ID não encontrado.");
+		userService.deleteById(id);
+		return ResponseEntity.ok(UserDto.converterToUser(userOptional));
+	}
 
-		return ResponseEntity.notFound().build();
+	// ------------------ Alterar Role USer --------------------------------
+
+	// metodo para o admin setar/mudar a role para admin, user,
+	@PatchMapping("/role/{id}")
+	@Transactional
+	public ResponseEntity<UserDto> updateUserRole(@PathVariable Long id, @RequestBody String roleName) {
+		Optional<User> userOptional = userService.findById(id);
+		if (!userOptional.isPresent())throw new ResourceNotFoundException("ID não encontrado.");
+		Optional<Role> roleOptional = roleService.findByName(roleName);
+		if(!roleOptional.isPresent())throw new ResourceNotFoundException("Role não encontrado.");
+		
+		User user = userService.updateRole(userOptional, roleOptional);
+		return ResponseEntity.ok(new UserDto(user));
+
+		// return ResponseEntity.notFound().build();
 	}
 
 }
