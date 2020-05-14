c4bfde773cf34a3d9b44b990024a62fd2077f3b0
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-05-14 19:22:53 -0300

    Adicionada query para contar a frequencia de logs

--- src/main/java/com/javawomen/errorcenter/repository/LogRepository.java
+++ src/main/java/com/javawomen/errorcenter/repository/LogRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -14,8 +15,7 @@ import com.javawomen.errorcenter.model.Log;
 @Repository
 public interface LogRepository extends JpaRepository<Log, Long> {
 	 
-	//Optional<Log> findById(Long id);
-	
+	//Optional<Log> findById(Long id);	
 	
 	Page<Log> findByEnvironmentName(String nameEnvironment, Pageable paginacao);
 	
@@ -26,5 +26,33 @@ public interface LogRepository extends JpaRepository<Log, Long> {
 	List<Log> findByOrigin(String origin);
 
 	List<Log> findByDescription(String description);
+	
+	
+	
+	
+	//--------------     TESTAR     ------------------------
+	//pegar um log e devolver o numero de vezes que ele aparece no banco
+	@Query
+	("select count(p) from Log p "
+			+ "join p.level le "
+			+ "join p.environment en "
+			+ "where le.name = :levelName and en.name =:environmentName "
+			+ "and p.origin = :originName and p.description = :descriptionName")
+	Long countByAllAttribute(String levelName, String environmentName, String originName, String descriptionName);
+	
+	//pegar todos e trazer ordenado por frequencia
+	//List<Log> findAll();
+	
+	//pegar todos de um ambiente e trazer ordenado por frequencia
+	//List<Log> findByEnvironmentName(String environmentName);
+	
+
+	
+	
+	
+	
+
+	
+	
 	
 }
