9b0330223aa44f6c4f13ddf143c23d0cbba1fa58
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-05-14 19:18:55 -0300

    Adicionada classe RoleService

---
+++ src/main/java/com/javawomen/errorcenter/service/RoleService.java
@@ -0,0 +1,40 @@
+package com.javawomen.errorcenter.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.javawomen.errorcenter.model.Role;
+import com.javawomen.errorcenter.repository.RoleRepository;
+
+@Service
+public class RoleService {
+
+	@Autowired
+	RoleRepository roleRepository;
+	
+	
+	public List<Role> findAll() {
+		return roleRepository.findAll();
+	}
+
+	public Optional<Role> findById(Long id) {
+		return roleRepository.findById(id);
+	}
+
+	public Role save(Role object) {
+		return roleRepository.save(object);
+	}
+
+	public void deleteById(Long id) {
+		roleRepository.deleteById(id);		
+	}
+
+	public Optional<Role> findByName(String roleName) {
+		return roleRepository.findByRoleName(roleName);
+	}
+}
