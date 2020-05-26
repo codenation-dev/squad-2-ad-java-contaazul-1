abb736ca81bd0af9f90ceb43ce570c9f7c9ccba6
Karina Padilha <kdpadilha@gmail.com>
2020-05-26 10:47:40 -0300

    Classes ajustadas com interface.

--- src/main/java/com/javawomen/errorcenter/service/EnvironmentService.java
+++ src/main/java/com/javawomen/errorcenter/service/EnvironmentService.java
@@ -1,60 +1,33 @@
 package com.javawomen.errorcenter.service;
- 
+
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.javawomen.errorcenter.controller.dto.EnvironmentDto;
 import com.javawomen.errorcenter.controller.form.EnvironmentForm;
 import com.javawomen.errorcenter.model.Environment;
-import com.javawomen.errorcenter.repository.EnvironmentRepository;
 
-@Service
-public class EnvironmentService{// implements ServiceInterface<Environment>{
+public interface EnvironmentService {
+	public List<Environment> findAll();
 
-	@Autowired
-	private EnvironmentRepository environmentRepository;
+	public Optional<Environment> findById(Long id);
 
-	public List<Environment> findAll() {
-		return environmentRepository.findAll();
-	}
+	public Environment save(Environment object);
 
-	public Optional<Environment> findById(Long id) {
-		return environmentRepository.findById(id);
-	}
+	public void deleteById(Long id);
 
-	public Environment save(Environment object) {
-		return environmentRepository.save(object);
-	}
-
-	public void deleteById(Long id) {
-		environmentRepository.deleteById(id);		
-	}
-
-	public Optional<Environment> findByName(String nameEnvironment) {
-		return environmentRepository.findByName(nameEnvironment);
-	}
+	public Optional<Environment> findByName(String nameEnvironment);
 	
 	//--------------- métodos que devolvem um dto ------------
 	
-	public EnvironmentDto converterToEnvironment(Environment environment) {			
-		return new EnvironmentDto(environment);
-	}
+	public EnvironmentDto converterToEnvironment(Environment environment);
 	
-	public List<EnvironmentDto> converter(List<Environment> environments) {
-		return environments.stream().map(EnvironmentDto::new).collect(Collectors.toList());
-	}
+	public List<EnvironmentDto> converter(List<Environment> environments);
 
-	public EnvironmentDto converterToEnvironment(Optional<Environment> environmentOptional) {
-		return converterToEnvironment(environmentOptional.get());
-	}
+	public EnvironmentDto converterToEnvironment(Optional<Environment> environmentOptional);
 
 	//------------------- métodos que devolvem um FORM --------------------
 	
-	public Environment converter(EnvironmentForm form) {
-			return new Environment(form.getName());
-	}
+	public Environment converter(EnvironmentForm form);
 }
--- src/main/java/com/javawomen/errorcenter/service/RoleService.java
+++ src/main/java/com/javawomen/errorcenter/service/RoleService.java
@@ -4,56 +4,27 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.javawomen.errorcenter.controller.dto.RoleDto;
 import com.javawomen.errorcenter.controller.form.RoleForm;
 import com.javawomen.errorcenter.model.Role;
-import com.javawomen.errorcenter.repository.RoleRepository;
 
-@Service
-public class RoleService {
+public interface RoleService {
+		
+	public List<Role> findAll();
 
-	@Autowired
-	RoleRepository roleRepository;
+	public Optional<Role> findById(Long id);
+
+	public Role save(Role object);
+
+	public void deleteById(Long id);
+
+	public Optional<Role> findByName(String roleName);
 	
+	public RoleDto converterToRole(Optional<Role> roleOptional);
 	
-	public List<Role> findAll() {
-		return roleRepository.findAll();
-	}
+	public RoleDto converterToRole(Role role);
 
-	public Optional<Role> findById(Long id) {
-		return roleRepository.findById(id);
-	}
+	public List<RoleDto> converter(List<Role> roles);
 
-	public Role save(Role object) {
-		return roleRepository.save(object);
-	}
-
-	public void deleteById(Long id) {
-		roleRepository.deleteById(id);		
-	}
-
-	public Optional<Role> findByName(String roleName) {
-		return roleRepository.findByRoleName(roleName);
-	}
-	
-	//--------------- métodos que devolvem um dto ------------
-	public RoleDto converterToRole(Optional<Role> roleOptional) {
-		return converterToRole(roleOptional.get());
-	}
-	
-	public RoleDto converterToRole(Role role) {
-		return new RoleDto(role);
-	}
-
-	public List<RoleDto> converter(List<Role> roles) {
-		return roles.stream().map(RoleDto::new).collect(Collectors.toList());
-	}
-
-	//------------------- métodos que devolvem um FORM --------------------
-	public Role converter(RoleForm form) {
-		return new Role(form.getRoleName());
-	}
+	public Role converter(RoleForm form);
 }
