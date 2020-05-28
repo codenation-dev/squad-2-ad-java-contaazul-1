87a34748b066b3acf5c617bf9bc6374edd241174
Alice Borges <aliceborges@outlook.com.br>
2020-05-27 22:50:40 -0300

    Novo teste para usuário realizado

--- src/test/service/com/javawomen/errorcenter/test/service/TestUserService.java
+++ src/test/service/com/javawomen/errorcenter/test/service/TestUserService.java
@@ -4,10 +4,14 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Optional;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.javawomen.errorcenter.model.User;
@@ -17,10 +21,25 @@ import com.javawomen.errorcenter.service.UserService;
 @RunWith(MockitoJUnitRunner.class)
 public class TestUserService {
 	@Mock
-	private UserRepository userRepository;
+	UserRepository userRepository;
 	
 	@InjectMocks
-	private UserService userService;
+	UserService userService;
+	
+	UserMock mock = new UserMock();
+	
+	@Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+	
+	@Test
+	public void registerUser() {
+		User user = mock.createUser(1);
+		Mockito.when(userRepository.save(Mockito.any(User.class))).thenReturn(user);
+		userRepository.save(user);
+		Assert.assertSame(mock.getListaForm().get(3).getName(), user.getName());
+	}
 	
 	@Test
 	public void findUserById() {
