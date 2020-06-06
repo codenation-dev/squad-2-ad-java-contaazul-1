3ab0166716377661ba0b8479d4e10a0fbbf1dc42
marleiSilveira <marlei.mmsilveira@gmail.com>
2020-06-06 15:14:59 -0300

    add build pra deploy no tomcat

--- src/main/java/com/javawomen/errorcenter/ErrorCenterApplication.java
+++ src/main/java/com/javawomen/errorcenter/ErrorCenterApplication.java
@@ -2,6 +2,8 @@ package com.javawomen.errorcenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -9,10 +11,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableSwagger2
-public class ErrorCenterApplication {
+public class ErrorCenterApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ErrorCenterApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ErrorCenterApplication.class);
+    }
 }
